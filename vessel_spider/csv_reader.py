9585bbfb97543c8fb012cdd738093cf0a7e98dcd
Stefan Langenbach <stefan.buenten@gmail.com>
2018-01-21 16:16:08 +0100

    Remove csv_reader.py
    Logic has been moved to vessel_spider.py

--- vessel_spider/csv_reader.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-# load input file
-data = list()
-
-with open("../data/unique_vessel.csv", newline="") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        data.append(row["Vessel_name"])
