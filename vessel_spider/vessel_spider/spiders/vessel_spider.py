ba4f2e9bfeeebbccd88021566e079c391d729e21
Stefan Langenbach <stefan.buenten@gmail.com>
2018-01-21 16:12:23 +0100

    Add start_urls generation from file, improved parsing, cleaning and output

--- vessel_spider/vessel_spider/spiders/vessel_spider.py
+++ vessel_spider/vessel_spider/spiders/vessel_spider.py
@@ -1,20 +1,39 @@
 import scrapy
+import csv
 
 class VesselSpider(scrapy.Spider):
     name = "vessel"
     allowed_domains = ["marinetraffic.com"]
-    start_urls = [
-        "https://www.marinetraffic.com/en/ais/index/search/all?keyword=9632143" # TODO: implement start_url generator
-    ]
+    start_urls = []
+
+    # generate start_urls from csv file or use defaults
+    def __init__(self, filename):
+        if filename:
+            with open(filename, newline="") as f:
+                reader = csv.DictReader(f)
+                base_url = "https://www.marinetraffic.com/en/ais/index/search/all?keyword="
+                self.start_urls = [base_url + str(row["Vessel_name"]) for row in reader]
+        else:
+            self.start_urls = [
+                "https://www.marinetraffic.com/en/ais/index/search/imo?keyword=9632143",
+                "https://www.marinetraffic.com/en/ais/index/search/all?keyword=101260"
+            ]
 
     def parse(self, response):
-        # proceed to further pages
-        for page_url in response.css("a[class = search_index_link]::attr(href)").extract():
-            page_url = response.urljoin(page_url)
-            yield scrapy.Request(url=page_url, callback=self.parse)
 
-        # extract vessel information TODO: parse data into proper JSON
-        for li in response.css("div[id = vessel_details_general]"):
-            yield {
-                "row": li.css("span").extract()
-            }
+        # access search result page
+        for result_page in response.css("a[class = search_index_link]::attr(href)"):
+            yield response.follow(result_page, callback=self.parse)
+
+        # parse general information TODO: implement xpath logic to deal with variations in  DOM
+        raw_keys = response.xpath("//*[@id='vessel_details_general']/div/ul/li/span/text()").extract()
+        raw_values = response.xpath("//*[@id='vessel_details_general']/div/ul/li/span/b/text()").extract()
+
+        # clean parsed data TODO: move cleaning logic to item pipeline
+        clean_keys = [e.replace(":", "").strip().lower() for e in raw_keys]
+        clean_values = [e.replace(" ", "_").lower() for e in raw_values]
+
+        # generate output
+        yield {
+            k:v for k, v in zip(clean_keys, clean_values)
+        }
