15050e75ca86f47989a7f73361e66b337c436c34
Stefan Langenbach <stefan.buenten@gmail.com>
2018-01-28 19:00:17 +0100

    Remove item definitions for now

--- vessel_spider/vessel_spider/items.py
+++ vessel_spider/vessel_spider/items.py
@@ -11,12 +11,4 @@ import scrapy
 class VesselSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    imo = scrapy.Field()
-    name = scrapy.Field()
-    mmsi = scrapy.Field()
-    vessel_type = scrapy.Field()
-    gross_tonnage = scrapy.Field()
-    summer_dwt = scrapy.Field()
-    build_year = scrapy.Field()
-    flag = scrapy.Field()
-    home_port = scrapy.Field()
+    pass
