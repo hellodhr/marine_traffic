a525895afc76ff83d0505bf77e8142da0005c306
Stefan Langenbach <stefan.buenten@gmail.com>
2018-01-28 20:10:05 +0100

    Add port spider
    Setup new scrapy project for scraping ports

---
+++ port_spider/port_spider/spiders/port_spider.py
@@ -0,0 +1,63 @@
+import os
+import fnmatch
+import csv
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class PortSpider(CrawlSpider):
+    name = "port"
+    allowed_domains = ["marinetraffic.com"]
+
+    # generate start_urls from csv file
+    for file in os.listdir("."):
+        if fnmatch.fnmatch(file, "*.csv"):
+            with open(file, newline="") as f:
+                reader = csv.reader(f)
+                # skip header
+                next(reader, None)
+                # build start_urls from second column of csv file
+                start_urls = ["https://www.marinetraffic.com/en/ais/index/search/all?keyword=" +
+                              str(row[1]) for row in reader]
+
+    # define rule to follow links in search result pages
+    rules = (
+        Rule(LinkExtractor(allow=("https://www.marinetraffic.com/en/ais/details/ports/*", ),
+                           restrict_css=("a[class = search_index_link]")), callback="parse_item"),
+    )
+
+    def parse_item(self, response):
+        """
+        Given an url, parse general content tab and return its values as Python dict
+
+        @url https://www.marinetraffic.com/en/ais/index/search/all?keyword=9632143
+        @returns items 1
+        @returns requests 0 0
+        @scrapes
+        """
+
+        # parse general information
+        info_vessels = response.xpath("//table[@id='currvess']")
+
+        # raw data
+        raw_keys = info_vessels.xpath(".//a/text()").extract()
+        raw_values = info_vessels.xpath("//a/@href").extract()
+
+        # parse keys
+        # raw_keys = list()
+        # for span in info_general.xpath(".//span/text()"):
+        #     raw_keys.append(span.extract())
+        #
+        # # parse values
+        # raw_values = list()
+        # for span in info_general.xpath(".//span/b/text()"):
+        #     raw_values.append(span.extract())
+        #
+        # # clean parsed data TODO: move cleaning logic to item pipeline
+        # clean_keys = [e.replace(":", "").replace(" ", "_", 1).strip().lower() for e in raw_keys]
+        # clean_values = [e.replace(" ", "_", 1).strip().lower() for e in raw_values]
+        #
+        # # generate output TODO: use scrapy items instead of python dicts
+        # yield {
+        #     k:v for k, v in zip(clean_keys, clean_values)
+        # }
